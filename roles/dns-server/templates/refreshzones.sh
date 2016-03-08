f0fc140c6c94af340c6b283024674ac5b63c6412
Tuux <tuxa@rtnp.org>
2016-03-08 14:14:06 +0100

    Doc update

--- roles/dns-server/templates/refreshzones.sh
+++ roles/dns-server/templates/refreshzones.sh
@@ -3,20 +3,20 @@
 # refreshzones v0.4 refresh tinydns zone files over ssh
 # (c) Jérôme Ornech <tuux@rtnp.org>
 #
-#   This package is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
+# This package is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# It script has been originaly white by:
-# The Plone® CMS — Open Source Content Management System is © 2000-2016 by the Plone Foundation et al.
+# It script has been originaly white by The Plone® CMS
 # https://plone.lucidsolutions.co.nz/linux/dns/creating-a-djb-tiny-dns-primary-secondary-server
 # refreshzones 28 2008-05-04 15:08:08
 #
-# The script have been modified and be coupled with a Makefile generate by Ansible via Jinja template
+# The script have been modified and be coupled with a Makefile generate by
+# Ansible via Jinja templateb
 # see https://raw.githubusercontent.com/Tuuux/galaxie/master/roles/dns-server/templates/Makefile.j2
-# The Makefile is suppose to deal with ssh for transfert zone files to secondary server
-# see https://cr.yp.to/djbdns/run-server-bind.html
+# The Makefile is suppose to deal with ssh for transfert zone files to
+# secondary server. see https://cr.yp.to/djbdns/run-server-bind.html
 #
 # The script support multiple primary and secondary domains.
 # This works with a standard "ndjbdns" directory structure.
@@ -38,25 +38,29 @@
 #       +- ...
 #  +- /var/log/tinydns.log
 #
-# Any exisiting  'data' file MUST be moved into the ./zones/primary.d/ directory (or it will be lost).
+# Any exisiting  'data' file MUST be moved into the ./zones/primary.d/
+# directory (or it will be lost).
 #
 # Place primary domain 'data' zone files into the ./zones/primary.d directory,
 # with an extensions of '.data'. (e.g. ./zones/primary.d/domain.eu.org.data)
 # These files must be manually edited.
 #
-# Domains for which the tinydns server will act as a secondary, should be automatically be receive
-# into the ./zones/secondary.d directory by a other server via ssh.
+# Domains for which the tinydns server will act as a secondary, should be
+# automatically be receive into the ./zones/secondary.d directory by a other
+# server via ssh.
 #
-# Prior to performing any things, the script will parse the ./zones/primary.d/ directory for search
-# zone files (extensions '.data') and generate a SOA serial number with the epoch date of the last modification
-# for each zone files, and write a other file with extensions '.data.tosend' it will be use
+# Prior to performing any things, the script will parse the ./zones/primary.d/
+# directory for search zone files (extensions '.data') and generate a
+# SOA serial number with the epoch date of the last modification for each zone
+# files, and write a other file with extensions '.data.tosend' it will be use
 # by the Makefile in case of ssh transfert.
 #
-# The primary zone files (e.g. ./zones/primary.d/*) SHOULD have a SOA record ('Z') with the serial number
-# field ('ser') empty. This script will replace the serial number of each primary zone files with it own
-# last modification epoch date. You
+# The primary zone files (e.g. ./zones/primary.d/*) SHOULD have a
+# SOA record ('Z') with the serial number field ('ser') empty. This script will
+# replace the serial number of each primary zone files with it own last
+# modification epoch date.
 #
-#  e.g. 'Zdomain.eu.org:a.ns.domain.eu.org.:hostmaster.domain.eu.org.::::::'
+# e.g. 'Zdomain.eu.org:a.ns.domain.eu.org.:hostmaster.domain.eu.org.::::::'
 #
 # see https://cr.yp.to/djbdns/tinydns-data.html
 
