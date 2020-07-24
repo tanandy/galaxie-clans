1609682a8eedfbe002f19f303662a39f5751f43c
Aurélien Maury <cto@wescale.fr>
2020-07-24 10:35:40 +0200

    wip

---
+++ docs/_ref_backup_structure.md
@@ -0,0 +1,12 @@
+[Galaxie-Clans Documentation](README.md) / [Reference Guides](_REF__.md)
+
+# Backup Structure
+
+## Directory structure
+
+```
+backups/
+  {{ inventory_hostname }}/
+    {{ role_name }}/
+      {{ date }}/
+```
---
+++ plugins/modules/gather_zone.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: gather_zone
+
+short_description: Reads a bind dns zone file
+
+version_added: "2.9"
+
+description:
+    - "This is my longer description explaining my test module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the test module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - Your Name (@yourhandle)
+'''
+
+EXAMPLES = '''
+# Pass in a message
+- name: Test with a message
+  my_test:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_test:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_test:
+    name: fail me
+'''
+
+RETURN = '''
+original_message:
+    description: The original name param that was passed in
+    type: str
+    returned: always
+message:
+    description: The output message that the test module generates
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from dnszone import dnszone
+
+TF_ALLOWED_RECORD_TYPES = [
+    'A',
+    'AAAA',
+    'CAA',
+    'CNAME',
+    'MX',
+    'NAPTR',
+    'NS',
+    'PTR',
+    'SOA',
+    'SPF',
+    'SRV',
+    'TXT'
+]
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        name=dict(type='str', required=True),
+        path=dict(type='str', required=True),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        zone=dict()
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    zone = dnszone.zone_from_file(module.params['name'], module.params['path'])
+    result['zone']['name'] = module.params['name']
+    result['zone']['data'] = dict()
+
+
+    for name, name_value in zone.get_names().items():
+        result['zone']['data'][name] = []
+        for rtype in TF_ALLOWED_RECORD_TYPES:
+            if name_value.records(rtype):
+                result['zone']['data'][name].append({
+                    'type': rtype,
+                    'ttl': name_value.ttl,
+                    'value': name_value.records(rtype).items
+                })
+
+
+                
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
+
+
+
